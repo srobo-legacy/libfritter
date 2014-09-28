ef5aaa246754be59dcfed2895beab56afa2835a5
Peter Law <PeterJCLaw@gmail.com>
2014-09-28 16:27:57 +0100

    Add a test for the preview script
    It turns out I'd let that get out of sync. This should ensure that
    it doesn't get forgotten again.

---
+++ test/test_preview_script.py
@@ -0,0 +1,19 @@
+
+import os.path
+from subprocess import CalledProcessError, check_output, STDOUT
+
+from .tests_helpers import root, assert_template_path
+
+def test_script():
+    script = os.path.join(root(), 'preview')
+    template = assert_template_path('template-1')
+
+    preview = None
+
+    try:
+        preview = check_output([script, template], stderr=STDOUT).decode('utf-8')
+    except CalledProcessError as cpe:
+        print(cpe.output)
+        raise
+
+    assert 'error' not in preview.lower()
