a6917596c6b19ef240f8114b1a2324ede62d5ab8
Peter Law <PeterJCLaw@gmail.com>
2014-10-05 17:24:09 +0100

    Add feedback within the DB creation script

--- scripts/make_db.sh
+++ scripts/make_db.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 DB_PATH="$1"
+if [ -z "$DB_PATH" ]
+then
+    name=`basename "$0"`
+    echo "Usage: $name path/to/database.db"
+    exit 1
+fi
+echo "Creating a libfritter compatible sqlite database at '$DB_PATH'."
 sqlite3 "$DB_PATH" "CREATE table IF NOT EXISTS outbox (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,\
                                                        toaddr                VARCHAR(256),\
                                                        template_name         VARCHAR(256),\
